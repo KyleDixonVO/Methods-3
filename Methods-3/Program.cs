cca071e09c6bea7c5b4ac9bc24d31846ce1ccdf2
KyleDixonVO <KyleDixon@hotmail.ca>
2021-09-27 13:19:39 -0300

    Infinite Loop

--- Methods-3/Program.cs
+++ Methods-3/Program.cs
@@ -8,8 +8,20 @@ namespace Methods_3
 {
     class Program
     {
+
+        static int count = 0;
+        static void Loop()
+        {
+            count = count + 1;
+            Console.WriteLine("This is Loop." + count);
+            Loop();
+        }
+        
         static void Main(string[] args)
         {
+            Loop();
+
+            Console.ReadKey(true);
         }
     }
 }
